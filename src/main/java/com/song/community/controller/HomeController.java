c7a7245444a98548d07024e2a15f67aa45bfdb4f
kyoni <syq278872032@gmail.com>
2022-02-26 22:41:37 +0800

    统一处理异常

--- src/main/java/com/song/community/controller/HomeController.java
+++ src/main/java/com/song/community/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import sun.security.krb5.internal.PAForUserEnc;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -48,5 +49,13 @@ public class HomeController {
         
         model.addAttribute("discussPosts", discussPostVoList);
         return "/index";
+    }
+
+    /**
+     * 错误页
+     */
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage () {
+        return "/error/500";
     }
 }
